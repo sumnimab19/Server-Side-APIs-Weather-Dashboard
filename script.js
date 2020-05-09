033d3f8ffa94c1610b0cce1b87bf1de5d5b6ced7
Sumnima B <sumnimabhandari@gmail.com>
2020-05-08 19:38:42 -0700

    Fixed forcast URL, fixed date formatting, added css code

--- script.js
+++ script.js
@@ -4,17 +4,22 @@ var storedValues = [];
 var APIKey = "644e2fd49d4e3e04ae6b482ca8428be6";
 
 searchBtn.on("click", getData);
-var lat;
-var lon;
+// var lat;
+// var lon;
 
 getValueFromLocalStorage()
 
 function getData(){  
+  if(cityName !== null){
+
+    var latitude = 32.22
+    var longitude = -110.93
 
   // var APIKey = "644e2fd49d4e3e04ae6b482ca8428be6";
   var searchedCity = $("#citySearch").val().trim();
   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=" + APIKey;
-  var queryURLForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchedCity + "&appid=" + APIKey;
+  //var queryURLForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchedCity + "&appid=" + APIKey;
+  var queryURLForecast = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=hourly&appid=" + APIKey;
 
 
   $.ajax({
@@ -22,15 +27,40 @@ function getData(){
     method: "GET" 
   }).then(function(response) {
     
-  // console.log(response);
+  console.log(response);
 
       latValue = response.coord.lat;
      
       lonValue = response.coord.lon;
       
 
-      var date = response.dt;
-      $(".city").html(response.name + "  (" + date + ")");
+      var timeStamp = response.dt;
+
+     // JSON timestamp
+   console.log(timeStamp)
+    
+   
+    // Convert timestamp to milliseconds
+    var date = new Date(timeStamp*1000);
+  
+    // Months array
+var months_arr = ['1','2','3','4','5','6','7','8','9','10','11','12'];
+// Year
+  var year = date.getFullYear();
+ 
+  // Month
+  var month = months_arr[date.getMonth()];
+ 
+  // Day
+  var day = date.getDate();
+
+ 
+  // Display date time in MM-dd-yyyy h:m:s format
+  var convertedDate = month+'/'+day+'/'+year;
+
+
+
+      $(".city").html(response.name + "  (" + convertedDate + ")");
       $(".city").css("font-weight","Bold");
       var iconNum = response.weather[0].icon;
      
@@ -57,32 +87,60 @@ function getData(){
       url: queryURLForecast,
       method: "GET" 
     }).then(function(response) {
-       //console.log(response)
-        var iconNum = response.list[0].weather[0].icon;
+       console.log(response)
+        //var iconNum = response.list[0].weather[0].icon;
+        var iconNum = response.daily[0].weather[0].icon;
      
       var forecastURL = "http://openweathermap.org/img/wn/" + iconNum + "@2x.png";
       $(".weatherIconForecast").attr("src",forecastURL);
       $('.weatherIconForecast').width(50); 
       $('.weatherIconForecast').height(50);
-      var date = (response.list[0].dt_txt).slice(0,11);
-      var formattedDate = moment(date).format("L");
-      $(".dateForecast").html(formattedDate)
+
+      //var date = (response.list[0].dt_txt).slice(0,11);
+      var timeStamp = (response.daily[0].dt);
+
+      // Convert timestamp to milliseconds
+   var date = new Date(timeStamp*1000);
   
-      $(".humidityForecast").text("Humidity: " + response.list[0].main.humidity + "%");
+    //Months array
+var months_arr = ['1','2','3','4','5','6','7','8','9','10','11','12'];
+//Year
+  var year = date.getFullYear();
+ 
+ // Month
+  var month = months_arr[date.getMonth()];
+ 
+ // Day
+ var day = date.getDate();
+
+ 
+ // Display date time in MM-dd-yyyy h:m:s format
+ var convertedDate = month+'/'+day+'/'+year;
+
+
+ console.log(convertedDate);
+
+      //var formattedDate = moment(date).format("L");
+      $(".dateForecast").text(convertedDate)
+      
+  
+      $(".humidityForecast").text("Humidity: " + response.daily[0].humidity + "%");
       
       // Convert the temp to fahrenheit
-      var tempF = (response.list[0].main.temp - 273.15) * 1.80 + 32;
+      var tempF = (response.daily[0].temp.day - 273.15) * 1.80 + 32;
   
       // add temp content to html
       $(".tempForecast").text("Temp: " + tempF.toFixed(2) + " °F");
   
 
     })
+    
     getUVIndex();
 
     
-    saveDataToLocalStorage();
-    getValueFromLocalStorage();
+   // storeValuesToLocalStorage();
+   // addStoredValue();
+   // getValueFromLocalStorage();
     cityName.val("");
 
     
@@ -124,7 +182,7 @@ function getData(){
       $(".uvindex").text(uvIndexValue)
 
       
-      console.log(uvIndexValue);
+      //console.log(uvIndexValue);
       
       if ((uvIndexValue >= 0) && (uvIndexValue <= 2)) {
         $(".uvindex").addClass("lowUV");
@@ -140,6 +198,70 @@ function getData(){
     })
   })
 }
+}
 
+// ----------------------------------------------------------------------//
+
+
+// This function pulls data from local storage
+function getValueFromLocalStorage(){
+  var getData = JSON.parse(localStorage.getItem("cities"));
+
+  if (getData !== null) {
+    storedValues = getData;
+    var liEl = $("<li>");
+    liEl.addClass("list-group-item eachCity");
+    liEl.text(getData);
+    liEl.appendTo(".cityList");        
+  }
+}
+
+// This function saves initial and score values to the local storage
+function storeValuesToLocalStorage(){
+    localStorage.setItem("cities", JSON.stringify(storedValues));
+}
+
+
+// This function gets the data from local storage
+// function getValueFromLocalStorage(){
+//   var getData = JSON.parse(localStorage.getItem("cities"));
+//   if (getData !== null) {
+//       storedValues = getData;
+//       console.log(storedValues)
+//   }
+// }
+
+
+// This function stores user input initial value and score to the local storage
+function addStoredValue (){
+    var storedCities = {
+    city: cityName.val().toUpperCase(),
+    };
+    storedValues.push(storedCities); 
+    storeValuesToLocalStorage();
+} 
+
+
+
+
+
+
+// // This function saves input data to localstorage
+// function saveDataToLocalStorage(){
+      
+//           var inputCity = cityName.val().toUpperCase();
+//          // console.log(inputCity)
+//           localStorage.setItem("cities", JSON.stringify(inputCity));
+//       }
+  
+
+
+
+
+//  citySelect() 
+// function citySelect(){
+//   var cityClicked = $(".eachCity");
+//   cityClicked.on("click", getData);
+// }
 
 
