00aa5664f8550c935c62951f32326fb6d75e8c73
Sumnima B <sumnimabhandari@gmail.com>
2020-05-16 15:43:30 -0700

    Added comments and cleaned the code

--- script.js
+++ script.js
@@ -26,70 +26,68 @@ $( document ).ready(function() {
     
       latValue = response.coord.lat;
       lonValue = response.coord.lon;
-        
+      
+      // JSON date conversion to readable date format
       var timeStamp = response.dt;
       // Convert timestamp to milliseconds
       var date = new Date(timeStamp*1000);
-    
       // Months array
       var months_arr = ['1','2','3','4','5','6','7','8','9','10','11','12'];
       // Year
       var year = date.getFullYear();
-      
       // Month
       var month = months_arr[date.getMonth()];
-      
       // Day
       var day = date.getDate();
-
       // Display date time in MM-dd-yyyy h:m:s format
       var convertedDate = month+'/'+day+'/'+year;
 
+      // Display City Name and date in parenthesis
       $(".city").html(response.name + "  (" + convertedDate + ")");
       $(".city").css("font-weight","Bold");
       var iconNum = response.weather[0].icon;
       
+      // Dispaly weather icon
       var iconURL = "http://openweathermap.org/img/wn/" + iconNum + "@2x.png";
       $(".weatherIcon").attr("src",iconURL);
       $('.weatherIcon').width(50); 
       $('.weatherIcon').height(50);
-    
+      
+      // Display wind, humidity
       $(".wind").text("Wind: " + response.wind.speed + " MPH");
       $(".humidity").text("Humidity: " + response.main.humidity + "%");
-      $(".uvindex").text("UV Index: ")
+      //$(".uvindex").text("UV Index: ")
         
       // Convert the temp to fahrenheit
       var tempF = (response.main.temp - 273.15) * 1.80 + 32;
     
-      // add temp content to html
+      // Display of temparature
       $(".temp").text("Temparature: " + tempF.toFixed(2) + "°F");
     });
   
 
+    // ajax call to get latitute and longitude data
     $.ajax({
       url: queryURL,
       method: "GET" 
     }).then(function(response) {
-      
         latitude = response.coord.lat;
         longitude = response.coord.lon;
 
         var queryURLForecast = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=hourly&appid=" + APIKey;
 
-  
+        // ajax call to get forecast data based on latitude and longitude
         $.ajax({
             url: queryURLForecast,
             method: "GET" 
           }).then(function(response) {
-          
-          //   var divRow = $(".forecastRow");
-
-          // var dayArray = ["dayOne","dayTwo","dayThree","dayFour","dayFive"];
+            // Arrays to hold classes 
             var dateForecastArray = ["dateForecastOne","dateForecastTwo","dateForecastThree","dateForecastFour","dateForecastFive"];
             var weatherIconForecastArray = ["weatherIconForecastOne","weatherIconForecastTwo","weatherIconForecastThree","weatherIconForecastFour","weatherIconForecastFive"];
             var tempForecastArray = ["tempForecastOne","tempForecastTwo","tempForecastThree", "tempForecastFour","tempForecastFive"];
             var humidityForecastArray = ["humidityForecastOne","humidityForecastTwo","humidityForecastThree","humidityForecastFour","humidityForecastFive"];
-          
+            
+            // For Look gives 5 days data for forecast from the list of 16 days array
             for (var i = 0; i < 5; i++) {   
                 var timeStamp = (response.daily[i].dt);
                 var date = new Date(timeStamp*1000);
@@ -120,6 +118,7 @@ $( document ).ready(function() {
 
   } 
 
+  // This function gives UV Index and add classes which is then used to format the color of that field based on it's value.
   function getUVIndex(){
     var searchedCity = $("#citySearch").val().trim();
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=" + APIKey;
@@ -142,6 +141,8 @@ $( document ).ready(function() {
         var uvIndexValue = parseFloat(response.value);
         $(".uvlabel").text("UV Index: ");
         $(".uvindex").text(uvIndexValue);
+
+        // Added classes to change color of UV Index value 
         if ((uvIndexValue >= 1) && (uvIndexValue <= 2)) {
           $(".uvindex").removeClass("moderateUV highUV veryHighUV extremeUV");
           $(".uvindex").addClass("lowUV");
@@ -200,109 +201,98 @@ $( document ).ready(function() {
   }
 
 
-// Added event listener to li elements
+// Added event listener to li elements (City List that are saved in Local Storage from above)
   var cityClicked = $(".eachCity");
   cityClicked.on("click", function(){
     $(".cityList").load(location.href + " .cityList");
-        // Grabbing and storing the city value from the list
+        // Grabbing and storing the clicked city value from the list of cities
         var clickedCity = $(this).text();
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + clickedCity + "&appid=" + APIKey;
 
-    $.ajax({
-      url: queryURL,
-      method: "GET" 
-    }).then(function(response) {
-    
-      latValue = response.coord.lat;
-      lonValue = response.coord.lon;
+        $.ajax({
+          url: queryURL,
+          method: "GET" 
+        }).then(function(response) {
+          latValue = response.coord.lat;
+          lonValue = response.coord.lon;
+          
+          var timeStamp = response.dt;
+          // Convert timestamp to milliseconds
+          var date = new Date(timeStamp*1000);
       
-      var timeStamp = response.dt;
-    // Convert timestamp to milliseconds
-      var date = new Date(timeStamp*1000);
-  
-    // Months array
-    var months_arr = ['1','2','3','4','5','6','7','8','9','10','11','12'];
-    // Year
-      var year = date.getFullYear();
- 
-      // Month
-      var month = months_arr[date.getMonth()];
-    
-      // Day
-      var day = date.getDate();
+          // Months array
+          var months_arr = ['1','2','3','4','5','6','7','8','9','10','11','12'];
+          // Year
+          var year = date.getFullYear();
+          // Month
+          var month = months_arr[date.getMonth()];
+          // Day
+          var day = date.getDate();
+          // Display date time in MM-dd-yyyy h:m:s format
+          var convertedDate = month+'/'+day+'/'+year;
 
-      // Display date time in MM-dd-yyyy h:m:s format
-      var convertedDate = month+'/'+day+'/'+year;
-
-      $(".city").html(response.name + "  (" + convertedDate + ")");
-      $(".city").css("font-weight","Bold");
-      var iconNum = response.weather[0].icon;
-     
-      var iconURL = "http://openweathermap.org/img/wn/" + iconNum + "@2x.png";
-      $(".weatherIcon").attr("src",iconURL);
-      $('.weatherIcon').width(50); 
-      $('.weatherIcon').height(50);
-  
-      $(".wind").text("Wind: " + response.wind.speed + " MPH");
-      $(".humidity").text("Humidity: " + response.main.humidity + "%");
-      $(".uvindex").text("UV Index: ")
+          $(".city").html(response.name + "  (" + convertedDate + ")");
+          $(".city").css("font-weight","Bold");
+          var iconNum = response.weather[0].icon;
+        
+          var iconURL = "http://openweathermap.org/img/wn/" + iconNum + "@2x.png";
+          $(".weatherIcon").attr("src",iconURL);
+          $('.weatherIcon').width(50); 
+          $('.weatherIcon').height(50);
       
-      // Convert the temp to fahrenheit
-      var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+          $(".wind").text("Wind: " + response.wind.speed + " MPH");
+          $(".humidity").text("Humidity: " + response.main.humidity + "%");
+          $(".uvindex").text("UV Index: ");
+          
+          // Convert the temp to fahrenheit
+          var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+          $(".temp").text("Temparature: " + tempF.toFixed(2) + "°F");
+        });
   
-      // add temp content to html
-      $(".temp").text("Temparature: " + tempF.toFixed(2) + "°F");
-    })
-  
 
-    $.ajax({
-      url: queryURL,
-      method: "GET" 
-    }).then(function(response) {
-
-        latitude = response.coord.lat;
-        longitude = response.coord.lon;
-
-    var queryURLForecast = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=hourly&appid=" + APIKey;
-
-    
-    $.ajax({
-        url: queryURLForecast,
+      $.ajax({
+        url: queryURL,
         method: "GET" 
       }).then(function(response) {
-        
-        var dateForecastArray = ["dateForecastOne","dateForecastTwo","dateForecastThree","dateForecastFour","dateForecastFive"];
-        var weatherIconForecastArray = ["weatherIconForecastOne","weatherIconForecastTwo","weatherIconForecastThree","weatherIconForecastFour","weatherIconForecastFive"];
-        var tempForecastArray = ["tempForecastOne","tempForecastTwo","tempForecastThree", "tempForecastFour","tempForecastFive"];
-        var humidityForecastArray = ["humidityForecastOne","humidityForecastTwo","humidityForecastThree","humidityForecastFour","humidityForecastFive"];
-      
-        for (var i = 0; i < 5; i++) {  
-            var timeStamp = (response.daily[i].dt);
-            var date = new Date(timeStamp*1000);
-            var months_arr = ['1','2','3','4','5','6','7','8','9','10','11','12'];
-            var year = date.getFullYear();
-            var month = months_arr[date.getMonth()];
-            var day = date.getDate();
-            var convertedDate = month+'/'+day+'/'+year;
-            $("." + dateForecastArray[i]).text(convertedDate);
-    
-            var iconNum = response.daily[i].weather[0].icon;
-            var forecastURL = "http://openweathermap.org/img/wn/" + iconNum + "@2x.png";
-            $("." + weatherIconForecastArray[i]).attr("src",forecastURL);
-            $("." + weatherIconForecastArray[i]).width(50); 
-            $("." + weatherIconForecastArray[i]).height(50);
-    
-            var tempF = (response.daily[i].temp.day - 273.15) * 1.80 + 32;
-            $("." + tempForecastArray[i]).text("Temp: " + tempF.toFixed(2) + " °F");
-    
-            $("." + humidityForecastArray[i]).text("Humidity: " + response.daily[i].humidity + "%");  
-        }
+          latitude = response.coord.lat;
+          longitude = response.coord.lon;
+
+          var queryURLForecast = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=hourly&appid=" + APIKey;
+
+          $.ajax({
+              url: queryURLForecast,
+              method: "GET" 
+            }).then(function(response) {
+              var dateForecastArray = ["dateForecastOne","dateForecastTwo","dateForecastThree","dateForecastFour","dateForecastFive"];
+              var weatherIconForecastArray = ["weatherIconForecastOne","weatherIconForecastTwo","weatherIconForecastThree","weatherIconForecastFour","weatherIconForecastFive"];
+              var tempForecastArray = ["tempForecastOne","tempForecastTwo","tempForecastThree", "tempForecastFour","tempForecastFive"];
+              var humidityForecastArray = ["humidityForecastOne","humidityForecastTwo","humidityForecastThree","humidityForecastFour","humidityForecastFive"];
+            
+              for (var i = 0; i < 5; i++) {  
+                  var timeStamp = (response.daily[i].dt);
+                  var date = new Date(timeStamp*1000);
+                  var months_arr = ['1','2','3','4','5','6','7','8','9','10','11','12'];
+                  var year = date.getFullYear();
+                  var month = months_arr[date.getMonth()];
+                  var day = date.getDate();
+                  var convertedDate = month+'/'+day+'/'+year;
+                  $("." + dateForecastArray[i]).text(convertedDate);
+          
+                  var iconNum = response.daily[i].weather[0].icon;
+                  var forecastURL = "http://openweathermap.org/img/wn/" + iconNum + "@2x.png";
+                  $("." + weatherIconForecastArray[i]).attr("src",forecastURL);
+                  $("." + weatherIconForecastArray[i]).width(50); 
+                  $("." + weatherIconForecastArray[i]).height(50);
+          
+                  var tempF = (response.daily[i].temp.day - 273.15) * 1.80 + 32;
+                  $("." + tempForecastArray[i]).text("Temp: " + tempF.toFixed(2) + " °F");
+          
+                  $("." + humidityForecastArray[i]).text("Humidity: " + response.daily[i].humidity + "%");  
+              }
+            })
       })
-    })
     
     getUVIndex();  
-
-  
 
     function getUVIndex(){
       var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + clickedCity + "&appid=" + APIKey;
@@ -311,7 +301,6 @@ $( document ).ready(function() {
         url: queryURL,
         method: "GET" 
       }).then(function(response) {
-        
           var latValue = response.coord.lat;      
           var lonValue = response.coord.lon;
 
@@ -346,7 +335,3 @@ $( document ).ready(function() {
     }  
   });
 });
-
-function reloadPage(){
-  location.reload(true);
-}
